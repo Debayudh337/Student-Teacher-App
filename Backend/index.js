98fec2992e151fa423633a06c340fd23341f8e2b
Debayudh337 <debayudhkhag33@gmail.com>
2025-09-03 16:46:26 +0530

    faultybackend

--- Backend/index.js
+++ Backend/index.js
@@ -58,7 +58,7 @@ app.use((err, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use('/:catchAll(*)', (req, res) => {
   res.status(404).json({
     success: false,
     message: 'API endpoint not found'
